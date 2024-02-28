75147f88a17e5f6ab0d91c337f1f03d4210f298b
wandresonbarbosa <wandresonbarbosa@gmail.com>
2024-02-28 17:22:16 -0300

    up classe sintaxe 01-08

--- 02-iniciando-do-zero-com-php/02-08-estruturas-de-repeticao/index.php
+++ 02-iniciando-do-zero-com-php/02-08-estruturas-de-repeticao/index.php
@@ -8,11 +8,35 @@ fullStackPHPClassName("02.08 - Estruturas de repetição");
  */
 fullStackPHPClassSession("while, do while", __LINE__);
 
+$looping = 1;
+$while = [];
 
+while ($looping <= 5) {
+    $while[] = $looping;
+    $looping++;
+};
+
+var_dump($while);
+
+
+$looping = 5;
+$while = [];
+
+do {
+    $while[] = $looping;
+    $looping--;
+} while ($looping >= 1);
+
+
+var_dump($while);
 /*
  * [ for ] https://php.net/manual/pt_BR/control-structures.for.php
  */
 fullStackPHPClassSession("for", __LINE__);
+
+for ($i = 0; $i <= 10; $i++) {
+    echo "<p>{$i}</p>";
+}
 
 
 /**
@@ -21,8 +45,34 @@ fullStackPHPClassSession("for", __LINE__);
  */
 fullStackPHPClassSession("break, continue", __LINE__);
 
+for ($w = 1; $w <= 10; $w++) {
+    if ($w % 2 == 1) {
+        continue;
+    }
+
+    if($w >= 10) {
+        break;
+    }
+
+    echo "<p>Pulo + 2 :: {$w}</p>";
+}
 
 /**
  * [ foreach ] https://php.net/manual/pt_BR/control-structures.foreach.php
  */
 fullStackPHPClassSession("foreach", __LINE__);
+
+$array = [];
+for ($arr = 0; $arr <= 5; $arr++) {
+    $array[] = $arr;
+}
+
+var_dump($array);
+
+foreach ($array as $item) {
+    var_dump($item);
+}
+
+foreach ($array as $key => $value) {
+    var_dump("{$key} = {$value}");
+}
--- 02-iniciando-do-zero-com-php/02-09-closures-e-generators/index.php
+++ 02-iniciando-do-zero-com-php/02-09-closures-e-generators/index.php
@@ -7,6 +7,36 @@ fullStackPHPClassName("02.09 - Closures e generators");
  */
 fullStackPHPClassSession("closures", __LINE__);
 
+$myAge = function ($year) {
+    $age = date("Y") - $year;
+    return "<h5>Você tem {$age} anos!</h5>";
+};
+
+echo $myAge(1992);
+
+$priceBrl = function ($price) {
+    return number_format($price, 2, ",", ".");
+};
+
+echo "<p>O projeto custa R$ {$priceBrl(3600)}. Vamos fechar?</p>";
+
+
+$myCart = [];
+$myCart["totalPrice"] = 0;
+$cardAdd = function ($item, $qtd, $price) use (&$myCart) {
+    $myCart[$item] = $qtd * $price;
+    $myCart["totalPrice"] += $myCart[$item];
+};
+
+
+$cardAdd("Playstation 5", 1, 3500);
+$cardAdd("Nintendo Switch lite", 1, 1020);
+$cardAdd("Controle DualSense", 1, 380);
+$cardAdd("Memoria Ram 16Gb", 2, 500);
+
+
+var_dump($myCart);
+
 
 /*
  * [ generators ] https://php.net/manual/pt_BR/language.generators.overview.php
